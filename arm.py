ee30b5ab7b513149ae949a595d4984bbb52a7f0d
Salavat Magazov <s.magazov@gmail.com>
2023-07-05 11:24:48 +0100

    feature: add forward kinematics

--- arm.py
+++ arm.py
@@ -1,17 +1,84 @@
+import math
+
+"""
+SCARA-like kinematic conversion of geometry for Marlin.
+
+Raises:
+    Exception: _description_
+    Exception: _description_
+    Exception: _description_
+    Exception: _description_
+
+Returns:
+    _type_: _description_
+"""
+
 class Arm():
     
-    def __init__(self, shoulder, elbow, origin = (0,0)):
+    """Initialise the class with limb lengths and position of static pivot within the coordinate system.
+    """
+    def __init__(self, shoulder, elbow, origin_x = 0, origin_y = 0):
         if shoulder > 0:
-            self._shoulder = shoulder
+            self.__shoulder_length = shoulder
         else:
             raise Exception('Shoulder size cannot be negative.') 
         if elbow > 0:
-            self._elbow = elbow
+            self.__elbow_length = elbow
         else:
             raise Exception('Elbow size cannot be negative.') 
-        if len(origin) == 2:
-            self._origin = origin
-        else:
-            raise Exception('Origin must be a tuple of size two.') 
-        self._b_homed = False
+        self.__origin_x = origin_x
+        self.__origin_y = origin_y
+        self.__b_homed = False
+        self.__position_x = 0
+        self.__position_y = 0
+        self.__angle = 0
+        self.__shoulder_angle = 0
+        self.__elbow_angle = 0
+        self.__wrist_angle = 0
     
+    def __str__(self):
+        return f'x:{self.__current_position[0]}, y:{self.__current_position[1]}'
+
+    #TODO: find a way to handle situation where arm needs to be completely straight...
+    def forwardKinematics(self, x, y, wrist = 0, rate = 0) -> tuple(float, float, float, float):
+        if self.isPositionOK(x, y):
+            # Calculate elbow angle. Test for correct angle.
+            angle = (x^2 + y^2 - self.__shoulder_length^2 - self.__elbow_length^2)
+            angle = angle / (2*self.__shoulder_length*self.__elbow_length)
+            if (-1 <= angle) & (self.__elbow_length <= 1):
+                self.__elbow_angle = math.acos(angle)
+            else:
+                raise Exception('Target unreachable.')
+            # Calculate shoulder angle
+            angle = self.__elbow_length * math.sin(self.__elbow_angle)
+            angle = angle / (self.__shoulder_length + self.__elbow_length * math.cos(self.__elbow_angle))
+            angle = math.atan(angle)
+            self.__shoulder_angle = math.atan(y/x) - angle
+            # Calculate wrist angle
+            self.__wrist_angle = wrist - self.__shoulder_angle - self.__elbow_angle
+        return (self.__shoulder_angle, self.__elbow_angle, 0)
+    
+    def isPositionOK(self, x, y) -> bool:
+        reach = self.__shoulder_length + self.__elbow_length
+        position = math.sqrt(x^2 + y^2)
+        return (reach >= position)
+    
+    def getPosition(self) -> tuple[float, float]:
+        return (self.__
+    
+    def setPosition(self, x, y, angle) -> bool:
+        b_success = False
+        if self.isPositionOK(x, y):
+            try: 
+                self.forwardKinematics(x, y, angle):
+            except Exception as e:
+                raise e 
+            finally:
+                self.__position_x = x
+                self.__position_y = y
+                self.__angle = angle
+                b_success = True
+        else:
+            raise Exception('Position is not reachable.')
+        return b_success
+
